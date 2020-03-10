5b050c39490c53ac4f2d84aaa4b95b4c67aee204
Kun Zhang <kunzhang1988@gmail.com>
2020-03-10 11:52:54 +1000

    Add Youtube API

---
+++ src/apis/youtube.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+export default axios.create(
+    {
+        baseURL: 'https://www.googleapis.com/youtube/v3',
+        params: {
+            part: 'snippet',
+            maxResults: 5,
+            type: 'video',
+            key: process.env.REACT_APP_Youtube_API_KEY
+        }
+    }
+)
